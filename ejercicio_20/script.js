50776cc91eb52d1a284ee41488f1c0a759d977a8
Juan A <juanprofesor@proton.me>
2025-03-14 19:19:24 +0100

    Agregar configuración de Live Server y mejorar la estructura del HTML en el ejercicio 20

--- ejercicio_20/script.js
+++ ejercicio_20/script.js
@@ -7,3 +7,22 @@
 // 3. Agregar un event listener al destino para detectar 'dragover' y evitar el comportamiento por defecto.
 // 4. Agregar un event listener al destino para detectar 'drop' y mover el elemento.
 
+document.addEventListener("DOMContentLoaded", function () {
+  let drag = document.getElementById("drag");
+  drag.setAttribute("draggable", "true");
+  let drop = document.getElementById("drop");
+
+  drag.addEventListener("dragstart", function (event) {
+    event.dataTransfer.setData("text", event.target.id);
+  });
+
+  drop.addEventListener("dragover", function (event) {
+    event.preventDefault();
+  });
+
+  drop.addEventListener("drop", function (event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text");
+    drop.appendChild(document.getElementById(data));
+  });
+});
