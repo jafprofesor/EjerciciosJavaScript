212f16d570c9896ecf8cc81179f7449cedd8d5d2
Juan A <juanprofesor@proton.me>
2025-03-14 16:50:32 +0100

    Mejorar la solución del ejercicio 5 con comentarios y alternativas de implementación

--- ejercicio_5/solucion.js
+++ ejercicio_5/solucion.js
@@ -1,6 +1,16 @@
 // SOLUCIÓN DEL EJERCICIO 5: Eliminar elementos de una lista
-document.querySelectorAll(".eliminar").forEach(boton => {
-    boton.addEventListener("click", function() {
-        this.parentNode.remove();
-    });
+document.querySelectorAll(".eliminar").forEach((boton) => {
+  // Seleccionamos todos los botones con la clase "eliminar"
+  boton.addEventListener("click", function () {
+    // Añadimos un evento click a cada botón
+    this.parentNode.remove(); // Eliminamos el nodo padre del botón
+  });
 });
+
+// Sin utilizar forEach
+// const botones = document.querySelectorAll(".eliminar");
+// for (let i = 0; i < botones.length; i++) {
+//   botones[i].addEventListener("click", function () {
+//     this.parentNode.remove();
+//   });
+// }
